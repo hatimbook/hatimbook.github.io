44e65e7b316d4adfd21030762e242e5f902ef8d1
hatimbook <hatimbookwala@gmail.com>
2020-12-12 15:05:55 +0530

    adding factor selection
    reformatting controls placement. Adding factor selection and link to mathologer video.

--- timestable.js
+++ timestable.js
@@ -1,52 +1,60 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
-var speed = 10;
-var dualColor = false;
-var playaudio = false;
-var isRunning = false;
 var bgaudio = document.getElementById("bgaudio"); 
-		
+var factorlabel = document.getElementById("factorlabel");
+var points = document.getElementById("NumPts");
+
+var speed = 10;
 var factor = 1.000;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-document.getElementById("factorlabel").innerHTML = factor.toFixed(2);
+
+var dualColor = false;
+var audioenabled = false;
+var isRunning = false;
+factorlabel.innerHTML = factor.toFixed(2);
+
+var canvas_radius;
+var circle_radius;
 drawCircle();
+
 		
 function drawCircle() {
+	canvas_radius = canvas.height / 2;
+	ctx.translate(canvas_radius, canvas_radius);
+	
+	circle_radius = canvas_radius * 0.90;
 	ctx.beginPath();
 	ctx.lineWidth = "3";
-	ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+	ctx.arc(0, 0, circle_radius, 0 , 2 * Math.PI);
 	ctx.strokeStyle = "white";
 	ctx.stroke();
+	
+	ctx.translate(-canvas_radius, -canvas_radius);
 }
 
 function drawTT() {		
-	var numofpts = document.getElementById("NumPts").value;
+	var numofpts = points.value;
 	var delta = (2 * Math.PI) / numofpts;
 	
 	factor = factor < 100.000 ? factor + 0.001 : 1.000;
-	document.getElementById("factorlabel").innerHTML = factor.toFixed(2);
-			
-	ctx.translate(-radius, -radius);
-	ctx.clearRect(0, 0, 600, 600);
-	ctx.translate(radius, radius);
+	factorlabel.innerHTML = factor.toFixed(2);
+	
+	if(dualColor) {
+		var c1 = parseInt((10*factor * 2.500) % 250);
+		var c2 = parseInt(250 - ((10 *factor * 2.500)%250));
+	}
+
+	ctx.clearRect(0, 0, canvas.width-3, canvas.height-3);
+	ctx.translate(canvas_radius, canvas_radius);
 	
 	for (i = 0; i < numofpts; i++) {
 		var a = (i % numofpts) * delta;
 		var b = ((i * factor) % numofpts) * delta;
-		var pointA = CirclePoints(radius, a, [0, 0]);
-		var pointB = CirclePoints(radius, b, [0, 0]);
+		var pointA = CirclePoints(circle_radius, a, [0, 0]);
+		var pointB = CirclePoints(circle_radius, b, [0, 0]);
 		ctx.beginPath();
 		ctx.lineWidth = "1";
 		if(dualColor) {
-			if (i % 2 == 0) {
-				var r = parseInt((10*factor * 2.500) % 250);
-				var g = parseInt(250 - ((10 *factor * 2.500)%250));
-				ctx.strokeStyle = "rgb("+r+","+g+", 125)"; 
-			} else {
-				ctx.strokeStyle = "rgb("+g+","+r+", 125)"; 
-			}
+			ctx.strokeStyle = (i % 2) ? "rgb("+c1+","+c2+", 125)" : "rgb("+c2+","+c1+", 125)";
 		} else {
 			ctx.strokeStyle = "#f8f8ff";
 		}
@@ -54,6 +62,7 @@ function drawTT() {
 		ctx.lineTo(pointB.x, pointB.y);
 		ctx.stroke();
 	}
+	ctx.translate(-canvas_radius, -canvas_radius);
 	drawCircle();
 }
 
@@ -74,11 +83,27 @@ function CirclePoints(radius, angle, origin) {
 	return {x, y};
 }
 
-function audioplay(pause) {
-	if(pause) {
+function SingleDraw() {
+	if(!isRunning) {	
+		factor = parseFloat(document.getElementById("input_factor").value);
+		drawTT();
+	}
+}
+
+function audioplay() {
+	if (isRunning && audioenabled) {
+		bgaudio.play();
+	} else {
 		bgaudio.pause();
 	}
-	else if (playaudio) {
-		bgaudio.play();
+	
+}
+
+window.onload = window.onresize = function() {
+    if(window.innerWidth < window.innerHeight) {
+		canvas.width = canvas.height = window.innerWidth * 0.75;
+	} else {
+		canvas.width = canvas.height = window.innerHeight * 0.75;
 	}
+	drawCircle();
 }
